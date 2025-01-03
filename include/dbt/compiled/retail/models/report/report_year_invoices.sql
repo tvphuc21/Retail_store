c110042fdcb4189bb9eb10e20858a6bb4991334a
TruongVinhPhuc <tvphuc21@clc.fitus.edu.vn>
2025-01-03 17:51:59 +0700

    Project

---
+++ include/dbt/compiled/retail/models/report/report_year_invoices.sql
@@ -0,0 +1,9 @@
+SELECT
+  dt.year,
+  dt.month,
+  COUNT(DISTINCT fi.invoice_id) AS num_invoices,
+  SUM(fi.total) AS total_revenue
+FROM `crucial-cycling-439407-m4`.`retail`.`fct_invoices` fi
+JOIN `crucial-cycling-439407-m4`.`retail`.`dim_datetime` dt ON fi.datetime_id = dt.datetime_id
+GROUP BY dt.year, dt.month
+ORDER BY dt.year, dt.month
---
+++ include/dbt/run/retail/models/transform/dim_customer.sql
@@ -0,0 +1,25 @@
+
+  
+    
+
+    create or replace table `crucial-cycling-439407-m4`.`retail`.`dim_customer`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      WITH customer_cte AS (
+	SELECT DISTINCT
+	    to_hex(md5(cast(coalesce(cast(CustomerID as string), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(Country as string), '_dbt_utils_surrogate_key_null_') as string))) as customer_id,
+	    Country AS country
+	FROM `crucial-cycling-439407-m4`.`retail`.`raw_invoices`
+	WHERE CustomerID IS NOT NULL
+)
+SELECT
+    t.*,
+	cm.iso
+FROM customer_cte t
+LEFT JOIN `crucial-cycling-439407-m4`.`retail`.`country` cm ON t.country = cm.nicename
+    );
+  
